38da78b19b7f435443713a4d5a4928886601ff39
Neusbeer <neusbeer@gmail.com>
2019-11-21 00:57:40 +0100

    Changes to be committed:   deleted:    generate-ssh-key.sh

--- generate-ssh-key.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ssh-keygen -t rsa -b 4096 -N '' -C "neusbeer@gmail.com" -f ~/.ssh/id_rsa
-ssh-keygen -t rsa -b 4096 -N '' -C "neusbeer@gmail.com" -f ~/.ssh/github_rsa
-ssh-keygen -t rsa -b 4096 -N '' -C "neusbeer@gmail.com" -f ~/.ssh/mozilla_rsa
