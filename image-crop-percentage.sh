c6be476e2b0b7fe54119dc0ad121652d2c999e43
Neusbeer <neusbeer@gmail.com>
2021-01-02 09:34:21 +0100

    Changes to be committed: modified:   image-crop-percentage.sh

--- image-crop-percentage.sh
+++ image-crop-percentage.sh
@@ -13,12 +13,20 @@ for f in *.jpg; do
   size=`identify -format "%wx%h" $f`
   width=`echo $size | cut -f 1 -d "x"`
   height=`echo $size | cut -f 2 -d "x"`
+  width=$(($width * 10))
+  height=$(($height * 10))
   newwidth=$(($width-($width/100)*$1))
   newheight=$(($height-($height/100)*$1))
-  echo -e "\t\twidth:\t\t$width \theight:\t\t$height"
-  echo -e "\t\tnew width:\t$newwidth \tnew height:\t$newheight"
   diffwidth=$((($width-$newwidth)))
   diffheight=$((($height-$newheight)))
+  width=$(($width/10))
+  height=$(($height/10))
+  newwidth=$(($newwidth/10))
+  newheight=$(($newheight/10))
+  diffwidth=$(($diffwidth/10))
+  diffheight=$(($diffheight/10))
+  echo -e "\t\twidth:\t\t$width \theight:\t\t$height"
+  echo -e "\t\tnew width:\t$newwidth \tnew height:\t$newheight"
   echo -e "\t\tdifference:\t$diffwidth \tdifference:\t$diffheight"
   convert $f -gravity Center -crop "$newwidth"x"$newheight"+0+0 +repage "$outputdir"/"$f"
 done
